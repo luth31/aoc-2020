0361e8d35448667f95a3a6383f566e3ec5950c1e
Paul Munteanu <munteanu.paul.c@gmail.com>
2020-12-02 06:27:59 +0200

    Add solution for Day 1 part 2

--- day1.py
+++ day1.py
@@ -21,7 +21,15 @@ def first(input_path: str):
 
 
 def second(input_path: str):
-    pass
+    numbers = []
+    with open(input_path) as file:
+        lines = file.read().splitlines()
+        numbers = [int(i) for i in lines]
+    for idx, i in enumerate(numbers[:-2]):
+        for idy, j in enumerate(numbers[idx + 1:-1]):
+            for idz, k in enumerate(numbers[idy + 1:]):
+                if i + j + k == 2020:
+                    print(f'The answer is: {i * j * k}. {idx} {idy} {idz}')
 
 
 if __name__ == '__main__':
